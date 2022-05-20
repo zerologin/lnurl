8986020601b71046c53aa3c50c575cd480e6173a
Dolu <doludev@protonmail.com>
2022-05-20 13:49:44 +0200

    Add unit tests

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { bech32 } from "bech32";
 const limit = 1023;
 const prefix = "lnurl";
 
-function decode(lnurl: string) {
+function decode(lnurl: string): string {
   const decoded = bech32.decode(lnurl, limit);
   return Buffer.from(bech32.fromWords(decoded.words)).toString("utf8");
 }
 
-function encode(unencoded: string) {
+function encode(unencoded: string): string {
   let words = bech32.toWords(Buffer.from(unencoded, "utf8"));
-  return bech32.encode(prefix, words, limit);
+  return bech32.encode(prefix, words, limit).toUpperCase();
 }
 
 export default { decode, encode };
---
+++ tests/lnurl.spec.ts
@@ -0,0 +1,24 @@
+import { test } from "@japa/runner";
+import lnurl from "../src/index";
+
+test.group("LNURL", () => {
+  test("can encode", ({ expect }) => {
+    const unencoded =
+      "https://service.com/api?q=3fc3645b439ce8e7f2553a69e5267081d96dcd340693afabe04be7b0ccd178df";
+    const encoded = lnurl.encode(unencoded);
+
+    const expected =
+      "LNURL1DP68GURN8GHJ7UM9WFMXJCM99E3K7MF0V9CXJ0M385EKVCENXC6R2C35XVUKXEFCV5MKVV34X5EKZD3EV56NYD3HXQURZEPEXEJXXEPNXSCRVWFNV9NXZCN9XQ6XYEFHVGCXXCMYXYMNSERXFQ5FNS";
+    expect(encoded).toEqual(expected);
+  });
+
+  test("can decode", ({ expect }) => {
+    const unencoded =
+      "LNURL1DP68GURN8GHJ7UM9WFMXJCM99E3K7MF0V9CXJ0M385EKVCENXC6R2C35XVUKXEFCV5MKVV34X5EKZD3EV56NYD3HXQURZEPEXEJXXEPNXSCRVWFNV9NXZCN9XQ6XYEFHVGCXXCMYXYMNSERXFQ5FNS";
+    const decoded = lnurl.decode(unencoded);
+
+    const expected =
+      "https://service.com/api?q=3fc3645b439ce8e7f2553a69e5267081d96dcd340693afabe04be7b0ccd178df";
+    expect(decoded).toEqual(expected);
+  });
+});
