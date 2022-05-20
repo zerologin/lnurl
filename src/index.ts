345c3acdf817f78610406ae14a81697fb5fc2db3
Dolu <doludev@protonmail.com>
2022-05-20 16:38:40 +0200

    Fix compilation for esm and cjs

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { bech32 } from "bech32";
 const limit = 1023;
 const prefix = "lnurl";
 
-export const decode = (lnurl: string): string => {
+const decode = (lnurl: string): string => {
   const decoded = bech32.decode(lnurl, limit);
   return Buffer.from(bech32.fromWords(decoded.words)).toString("utf8");
-}
+};
 
-export const encode = (unencoded: string): string => {
+const encode = (unencoded: string): string => {
   let words = bech32.toWords(Buffer.from(unencoded, "utf8"));
   return bech32.encode(prefix, words, limit).toUpperCase();
-}
+};
+
+export default { decode, encode };
