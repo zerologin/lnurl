e89dc607b7386d274ce79703a98f802515b51ace
Dolu <doludev@protonmail.com>
2023-01-26 15:24:52 +0100

    test: add test UPPERCASE without protocol

--- tests/lnurl.spec.ts
+++ tests/lnurl.spec.ts
@@ -58,3 +58,19 @@ test("can decode with protocol UPPERCASE", ({ expect }) => {
   };
   expect(decoded).toEqual(expected);
 });
+
+test("can decode without protocol UPPERCASE", ({ expect }) => {
+  const encoded =
+    "LNURL1DP68GURN8GHJ7UM9WFMXJCM99E3K7MF0D3H82UNV9ASH2ARG8A6XZEEAD3HKW6TWYE4NZ0T9XY6R2WTYX3JNZCEJV56NGDTP8QUKGWRYVE3RWEF4VDNRVCMP8YCXZVNY8PNRVETYXGCNWDEJXFJK2VEJX5UXXCNZVVMRYVPKXGURZFNGD4SKX0TRV9JNXCM98QMRWV35XFNRXVNPXUENSCTRXEJRQCEKXPJK2ETXVSCNSERXXQMRZWRRXE3NQCTZXAJKYDRY8QENXERYVS6NSDRPXYMRZAJXAKZ";
+  const decoded = decode(encoded);
+
+  const expected =
+  {
+    decoded: "https://service.com/lnurl/auth?tag=login&k1=e1459d4e1c2e545a89d8dfb7e5cf6ca90a2d8f6ed217722ee3258cbbc6206281&hmac=cae3ce867242f32a738ac6d0c60eeefd18df0618c6c0ab7eb4d833ddd584a161",
+    domain: "service.com",
+    action: "",
+    k1: "e1459d4e1c2e545a89d8dfb7e5cf6ca90a2d8f6ed217722ee3258cbbc6206281",
+    tag: "login"
+  };
+  expect(decoded).toEqual(expected);
+});
